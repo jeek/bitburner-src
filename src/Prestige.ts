9e1d55eac10d47651143cbb8d6ed7acf098c9f3e
T.J. Eckman <jeek@jeek.net>
2024-05-24 04:29:28 -0400

    Cap level 12 prestige benefit

--- src/Prestige.ts
+++ src/Prestige.ts
@@ -223,7 +223,10 @@ export function prestigeSourceFile(isFlume: boolean): void {
   if (Player.sourceFileLvl(12) > 0) {
     Player.augmentations.push({
       name: AugmentationName.NeuroFluxGovernor,
-      level: Player.sourceFileLvl(12),
+      level: Math.min(
+        35842,
+        Player.bitNodeN === 12 ? Player.sourceFileLvl(12) : Math.min(50, Player.sourceFileLvl(12)),
+      ),
     });
   }
 
